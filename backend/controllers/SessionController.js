518769205685c766b153cffb7390fb0bf89adbcf
Dhruvil Patel <dhruvilpatel2002@gmail.com>
2024-04-21 19:25:01 +0530

    .

--- backend/controllers/SessionController.js
+++ backend/controllers/SessionController.js
@@ -1,5 +1,3 @@
-import dotenv from "dotenv";
-dotenv.config();
 import querystring from "querystring";
 import { Teacher } from "../model/Teacher.js";
 import { Student } from "../model/Student.js";
